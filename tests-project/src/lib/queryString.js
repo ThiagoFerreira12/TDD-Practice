db3eb0fc2a16ea37e799254615bdb2913677647f
Thiago Alencar <thiago.alencar@pontomais.com.br>
2022-08-18 18:27:07 -0300

    :recycle: Wip test

--- tests-project/src/lib/queryString.js
+++ tests-project/src/lib/queryString.js
@@ -1,5 +1,11 @@
+const keyValue = ([key, value]) => {
+  if (typeof value === 'object' && !Array.isArray(value)) {
+    throw new Error('Object not allowed');
+  }
+
+  return `${key}=${value}`;
+};
+
 module.exports.queryString = obj => {
-  return Object.entries(obj)
-    .map(([key, value]) => `${key}=${value}`)
-    .join('&');
+  return Object.entries(obj).map(keyValue).join('&');
 };
--- tests-project/src/lib/queryString.spec.js
+++ tests-project/src/lib/queryString.spec.js
@@ -1,12 +1,31 @@
-const { queryString } = require("./queryString");
+const { queryString } = require('./queryString');
 
-describe("Object to query string", () => {
-  test("should be create a valide query string", () => {
+describe('Object to query string', () => {
+  test('should be create a valide query string', () => {
     const obj = {
-      name: "Thiago",
-      profession: "Developer",
+      name: 'Thiago',
+      profession: 'Developer',
     };
 
-    expect(queryString(obj)).toBe("name=Thiago&profession=Developer");
+    expect(queryString(obj)).toBe('name=Thiago&profession=Developer');
+  });
+
+  test('should create valid query string with even when an array', () => {
+    const obj = {
+      name: 'Thiago',
+      abilities: ['Developer,React'],
+    };
+    expect(queryString(obj)).toBe('name=Thiago&abilities=Developer,React');
+  });
+
+  test('should throw an error when an object is passed', () => {
+    const obj = {
+      name: 'Thiago',
+      abilities: {
+        first: 'Developer',
+        second: 'React',
+      },
+    };
+    expect(() => queryString(obj)).toThrowError();
   });
 });

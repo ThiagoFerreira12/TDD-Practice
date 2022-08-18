39f425d011c71f0f4743cd3cd7dd30620d93708a
Thiago Alencar <thiago.alencar@pontomais.com.br>
2022-08-18 17:56:08 -0300

    :recycle: Wip test

--- tests-project/src/lib/queryString.js
+++ tests-project/src/lib/queryString.js
@@ -1,7 +1,5 @@
-module.exports.queryString = (obj) => {
-  const objectMap = Object.entries(obj).map((item) => {
-    return `${item[0]}=${item[1]}`;
-  });
-
-  return objectMap.join("&");
+module.exports.queryString = obj => {
+  return Object.entries(obj)
+    .map(([key, value]) => `${key}=${value}`)
+    .join('&');
 };
